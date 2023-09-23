057249529381d677fcf57cba0fbcfc7f9eed3e44
kuttubaj <iskakovkuttuubaj0207@gmail.com>
2023-09-23 17:07:38 +0600

    kkkk

--- src/Hero.java
+++ src/Hero.java
@@ -2,15 +2,24 @@ public class Hero {
     private int health;
     private int damage;
     private String teleport;
+
     public int getHealth() {
         return health;
     }
+
     public int getDamage() {
         return damage;
     }
+
     public String getTeleport() {
         return teleport;
     }
+
+    public Hero(int health, int damage) {
+        this.health = health;
+        this.damage = damage;
+    }
+
     public Hero(int health, int damage, String Teleport) {
         this.health = health;
         this.damage = damage;
